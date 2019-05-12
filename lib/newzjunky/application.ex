d2b8c0f1f3c7272b6110c481ad42c6bf478df2d3
Michael Symmes <msymmes@gmail.com>
2019-05-11 21:24:21 -0400

    remove stories before rebuild.

--- lib/newzjunky/application.ex
+++ lib/newzjunky/application.ex
@@ -5,6 +5,27 @@ defmodule Newzjunky.Application do
 
   use Application
 
+  @doc """
+      Logger.info("Populating stories")
+      HTTPoison.start()
+      r = HTTPoison.get!(Newzjunky.Config.url())
+      body = Jason.decode!(r.body)
+
+      stories =
+        body["articles"]
+        |> Enum.map(fn story ->
+          %Story{
+            title: story["title"],
+            url: story["url"],
+            urlToImage: story["urlToImage"],
+            description: story["description"],
+            content: story["content"],
+            author: story["author"]
+          }
+        end)
+
+      render(conn, "index.json", stories: stories)
+  """
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
@@ -15,6 +36,8 @@ defmodule Newzjunky.Application do
       # Starts a worker by calling: Newzjunky.Worker.start_link(arg)
       # {Newzjunky.Worker, arg},
     ]
+
+    :ets.new(:newzjunky, [:named_table])
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
